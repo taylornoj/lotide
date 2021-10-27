ca6e1d6c52aba194f89caee5401d776b806a642f
Taylor Noj <taylornoj@gmail.com>
2021-10-27 17:16:38 +0000

    created middle assignment

---
+++ middle.js
@@ -0,0 +1,21 @@
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  } for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2) === true) {
+    console.log(`✅Assertion Passed: ${array1} === ${array2}`);
+  } else console.log(`❌Assertion Failed: ${array1}} !== ${array2}`);
+};
+
+const middle = function(array) {
+  return 
+
+}
