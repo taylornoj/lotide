c9e4d3634725d033e806d6e1957b719212177287
Taylor Noj <taylornoj@gmail.com>
2021-10-27 03:26:35 +0000

    assignment completed

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -16,7 +16,3 @@ const assertArraysEqual = function(array1, array2) {
 };
 
 
-console.log(assertArraysEqual([1, 2, 3], [1, 2, 3]));
-console.log(assertArraysEqual([1, 2, 3], [3, 2, 1]));
-console.log(assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]));
-console.log(assertArraysEqual(["1", "2", "3"], ["1", "2", 3]));
