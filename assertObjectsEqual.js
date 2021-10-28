eb7e35aa80cfca3eca10a99d634534c7bc9aaf71
Taylor Noj <taylornoj@gmail.com>
2021-10-28 11:38:49 +0000

    corrected console.log assertions to include inspect

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -29,19 +29,21 @@ const eqObjects = function(object1, object2) {
 
 const assertObjectsEqual = function(actual, expected) {
   const inspect = require('util').inspect;
-  console.log(`Example label: ${inspect(actual)}`);
   if (eqObjects(actual, expected)) {
-    return console.log(`✅Assertion Passed: ${actual} === ${expected}`);
+    return console.log(`✅ Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`);
   } else
-  return console.log(`❌Assertion Failed: ${actual}} !== ${expected}`);
+  return console.log(`❌ Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`);
 };
 
+
+const ab = { a: "1", b: "2" };
+const ba = { b: "2", a: "1" };
+assertObjectsEqual(ab, ba)
+
+
+const abc = { a: "1", b: "2", c: "3" };
+assertObjectsEqual(ab, abc);
 
 const cd = { c: "1", d: ["2", 3] };
 const dc = { d: ["2", 3], c: "1" };
 assertObjectsEqual(cd, dc);
-
- const cd2 = { c: "1", d: ["2", 3, 4] };
- assertObjectsEqual(cd, cd2)
-      
-    
