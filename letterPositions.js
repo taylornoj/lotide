609ff91784690f0577c3a409aa398816c0f88f73
Taylor Noj <taylornoj@gmail.com>
2021-10-28 02:35:08 +0000

    finished code with tests

--- letterPositions.js
+++ letterPositions.js
@@ -19,9 +19,20 @@ const eqArrays = function(array1, array2) {
 
 const letterPositions = function(sentence) {
   const results = {};
-  for (let i = 0; i < sentence.length; i++)
-  return results
+  for (let i = 0; i < sentence.length; i++) {
+    if (sentence[i] !== " ") {
+      if (results[sentence[i]]) {
+        results[sentence[i]].push(i);
+      } else {
+        results[sentence[i]] = [i];
+      }
+    }
+  }
+  return results;
 };
+
+//test
 assertArraysEqual(letterPositions("hello").e, [1]);
-
-
+assertArraysEqual(letterPositions("hello").l, [2, 3]);
+assertArraysEqual(letterPositions("hello").h, [0]);
+assertArraysEqual(letterPositions("hello").o, [4]);
