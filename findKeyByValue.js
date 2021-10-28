7c5172fb0764ca4cb6b2b8181ef2d61fe3f03e69
Taylor Noj <taylornoj@gmail.com>
2021-10-28 02:58:17 +0000

    new function and tests

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -6,3 +6,21 @@ const assertEqual = function(actual, expected) {
   }
 };
 
+const findKeyByValue = function(object, value) {
+ // loop over object
+ // return key containing given value
+ // if no key with given value is found = undefined
+}
+
+
+
+
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
