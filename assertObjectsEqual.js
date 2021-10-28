e12def2397c9444e484655987770b5d024cefdbe
Taylor Noj <taylornoj@gmail.com>
2021-10-28 11:18:33 +0000

    creating assertObjectsEqual

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,3 +1,10 @@
+
+// const assertArraysEqual = function(array1, array2) {
+//   if (eqArrays(array1, array2) === true) {
+//     console.log(`✅Assertion Passed: ${array1} === ${array2}`);
+//   } else console.log(`❌Assertion Failed: ${array1}} !== ${array2}`);
+// };
+
 const eqObjects = function(object1, object2) {
   let obj1 = Object.keys(object1);
   let obj2 = Object.keys(object2);
@@ -15,8 +22,12 @@ const eqObjects = function(object1, object2) {
   return true;
 };
 
+
 const assertObjectsEqual = function(actual, expected) {
-  const inspect = require('util').inspect; 
+  const inspect = require('util').inspect;
   // Implement me!
 };
 console.log(`Example label: ${inspect(actual)}`);
+
+      console.log(`✅Assertion Passed: ${actual} === ${expected}`);
+      console.log(`❌Assertion Failed: ${actual}} !== ${expected}`);
