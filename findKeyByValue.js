26d5be1245f3fb17d6cff838dd1711cde55d536b
Taylor Noj <taylornoj@gmail.com>
2021-10-28 03:26:58 +0000

    completed function

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -7,13 +7,16 @@ const assertEqual = function(actual, expected) {
 };
 
 const findKeyByValue = function(object, value) {
+  let keys = Object.keys(object);
+ for (const key of keys) {
+   if (object[key] === value) {
+     return key;
+   }
+ }
+}
  // loop over object
  // return key containing given value
  // if no key with given value is found = undefined
-}
-
-
-
 
 
 const bestTVShowsByGenre = { 
