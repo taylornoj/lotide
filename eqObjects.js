7af3f228d92477d8a24556b0e1e346f5315a7baf
Taylor Noj <taylornoj@gmail.com>
2021-10-28 06:31:28 +0000

    linting done

--- eqObjects.js
+++ eqObjects.js
@@ -20,19 +20,19 @@ const eqArrays = function(array1, array2) {
 // will take in 2 objects and return t/f based on perfect match
 // return true if both objects have identical keys w/ values - otherwise false
 const eqObjects = function(object1, object2) {
-  let obj1 = Object.keys(object1)
-  let obj2 = Object.keys(object2)
+  let obj1 = Object.keys(object1);
+  let obj2 = Object.keys(object2);
   if (obj1.length !== obj2.length) { // checking length
     return false;
-  } 
-    for (let key of obj1) { 
-      if (Array.isArray(object1[key]) && (Array.isArray(object2[key]))) {
-        return eqArrays(object1[key], object2[key]);
-        }
-       if (object1[key] !== object2[key]) { //comparing object values
-        return false;
-      } 
+  }
+  for (let key of obj1) {
+    if (Array.isArray(object1[key]) && (Array.isArray(object2[key]))) {
+      return eqArrays(object1[key], object2[key]);
     }
+    if (object1[key] !== object2[key]) { //comparing object values
+      return false;
+    }
+  }
   return true;
 };
 
@@ -43,4 +43,4 @@ const dc = { d: ["2", 3], c: "1" };
 assertEqual(eqObjects(cd, dc), true);
 
 const cd2 = { c: "1", d: ["2", 3, 4] };
-assertEqual(eqObjects(cd, cd2), false)
+assertEqual(eqObjects(cd, cd2), false);
