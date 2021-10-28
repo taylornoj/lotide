141718bfd7fd912e318fba0df73ad791d4b39f46
Taylor Noj <taylornoj@gmail.com>
2021-10-28 02:57:12 +0000

    creating findKeyByValue.js

---
+++ findKeyByValue.js
@@ -0,0 +1,8 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
