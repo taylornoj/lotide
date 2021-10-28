c90db1450d3a3a80d9dbd2773a94d2f1f265484c
Taylor Noj <taylornoj@gmail.com>
2021-10-28 04:32:41 +0000

    tested for object length

--- eqObjects.js
+++ eqObjects.js
@@ -5,3 +5,21 @@ const assertEqual = function(actual, expected) {
     console.log(`❌Assertion Failed: ${actual} !== ${expected}`);
   }
 };
+// will take in 2 objects and return t/f based on perfect match
+// return true if both objects have identical keys w/ values - otherwise false
+const eqObjects = function(object1, object2) {
+  let obj1 = Object.keys(object1)
+  let obj2 = Object.keys(object2)
+  if (obj1.length !== obj2.length) {
+    return false;
+  }
+
+    
+};
+
+const ab = { a: "1", b: "2" };
+const ba = { b: "2", a: "1" };
+console.log(assertEqual(eqObjects(ab, ba))); // => true
+
+const abc = { a: "1", b: "2", c: "3" };
+console.log(assertEqual(eqObjects(ab, abc))); // => false
