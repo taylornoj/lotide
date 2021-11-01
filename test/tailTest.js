8d18ca28b2159861ffbc3f33296ba165b5641387
Taylor Noj <taylornoj@gmail.com>
2021-11-01 23:43:34 +0000

    M&C on tailTest

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,11 +1,15 @@
+const assert = require('chai').assert;
 const tail = require('../tail');
-const assertEqual = require('../assertEqual');
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
 
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+
+describe("#tail removes the first item of the array", () => {
+  it("returns [2, 3, 4] for [1, 2, 3, 4]", () => { 
+    assert.deepEqual(tail([1, 2, 3, 4]), [2, 3, 4]);
+  });
+
+  it("returns ['Lighthouse, Labs'] for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.deepEqual(tail(['Hello', 'Lighthouse', 'Labs']), ['Lighthouse', 'Labs']);
+  });
+  
+});
